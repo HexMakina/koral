e866ea7fda35c10913613e7f7633b838f8227c5f
sam <sammy@hexmakina.be>
2021-08-27 23:12:45 +0200

    koral app initial commit

---
+++ Controllers/HomeController.class.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace HexMakina\koral\Controllers;
+
+use \HexMakina\kadro\Auth\Operator;
+use \HexMakina\Format\Tempo\Dato;
+
+class HomeController extends \HexMakina\kadro\Controllers\KadroController
+{
+  public function home()
+  {
+    return 'home/dashboard';
+  }
+
+  public static function bootstrap($Controller, $Operator)
+  {
+  	if(!$Controller->box('StateAgent')->has_filter('date_start'))
+  	  $Controller->box('StateAgent')->filters('date_start', Dato::format($Controller->box('settings.app.time_window_start'), Dato::FORMAT));
+
+  	if(!$Controller->box('StateAgent')->has_filter('date_stop'))
+  	  $Controller->box('StateAgent')->filters('date_stop', Dato::format($Controller->box('settings.app.time_window_stop'), Dato::FORMAT));
+
+    self::common_viewport($Controller, $Operator);
+
+
+    $Controller->execute();
+  }
+
+
+  public static function common_viewport($Controller, $Operator)
+  {
+
+    //-------------------------------------------------------OPERATORS/WORKERS/TEAMS
+    $all_operators = Operator::filter();
+    $Controller->viewport('all_operators', $all_operators);
+
+    //-------------------------------------------------------ITEMS & TYPE
+  }
+}
+?>
---
+++ install.php
@@ -0,0 +1,97 @@
+<?php
+
+define('APP_BASE', __DIR__.'/');
+
+require_once APP_BASE.'lib/kadro/common.inc.php';
+
+use HexMakina\LocalFS\Text\TextFile;
+use HexMakina\LocalFS\FileSystem;
+use \HexMakina\Crudites\{Crudites,Database};
+use \HexMakina\kadro\Auth\{Operator, Permission, ACL};
+use \HexMakina\kadro\Controllers\{TradukoController};
+
+require_once 'configs/database.php';
+
+$database = new Database(
+  Crudites::connect($db_props['CONTENT']),
+  Crudites::connect($db_props['SCHEMA'], $db_props['SCHEMA']['name'])
+);
+
+Crudites::setDatabase($database);
+
+
+// init database
+$database_files = [
+  'structure' => [
+    'database_auth',
+    'database_i18n'
+  ],
+  'data' => [
+    'datadump_i18n_codes',
+    'datadump_i18n_labels'
+  ]
+];
+
+$conx = $database->contentConnection();
+
+echo '<pre>';
+foreach($database_files as $type => $filenames)
+{
+  echo(PHP_EOL."LOADING DATABASE $type");
+
+  foreach($filenames as $filename)
+  {
+    echo(PHP_EOL."EXECUTING $filename.sql");
+    $sql_dump = new TextFile(APP_BASE.'lib/kadro/BaseData/'.$filename.'.sql');
+    $res = $conx->exec("$sql_dump");
+    if($res === false)
+      throw new \Exception("EXECUTING $filename.sql FAILED");
+  }
+}
+
+echo(PHP_EOL."CREATING ROOT USER");
+$root_props = ['username' => 'koral', 'name' => 'Koral'];
+$conx->exec("INSERT INTO `kadro_operator` (`id`, `username`, `name`, `active`) VALUES (1, 'koral','Koral', 1);");
+
+$operator = Operator::one(1);
+$operator_id = $operator->get_id();
+
+echo(PHP_EOL."CREATING ROOT PASSWORD");
+$operator->password_change($operator->username());
+$operator->save($operator_id);
+
+echo(PHP_EOL.'CREATING ROOT PERMISSION');
+$permission = new Permission();
+$permission->set('name', 'root');
+$permission->save($operator_id);
+// vd($permission);
+
+echo(PHP_EOL.'CREATING ROOT ACCESS');
+$acl = ACL::allow_in($operator, $permission);
+// vd($acl);
+
+
+echo(PHP_EOL.'CREATING TRANSLATION FILES');
+$languages = TradukoController::init($box->get('settings.locale_data_path'));
+echo(PHP_EOL."LANGUAGES: " . implode(', ',$languages));
+
+
+echo(PHP_EOL.'CREATING .htaccess');
+if(!FileSystem::copy('.htaccess', '../.htaccess'))
+  echo(PHP_EOL.'FAILURE CREATING .htaccess');
+
+echo(PHP_EOL.'CREATING index.php');
+if(!FileSystem::copy('app_index.php', '../index.php'))
+  echo(PHP_EOL.'FAILURE CREATING index.php');
+
+echo(PHP_EOL.'CREATING configs FOLDER');
+if(exec('cp configs ../configs') === false)
+  echo(PHP_EOL.'FAILURE CREATING configs');
+
+echo(PHP_EOL.'INSTALLATION FINISHED');
+
+echo '</pre>';
+
+die('here');
+
+?>
