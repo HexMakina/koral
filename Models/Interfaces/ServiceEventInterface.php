360fe93682f488688ad4aece65c645a27c0c2b99
sam <sammy@hexmakina.be>
2021-08-27 23:37:11 +0200

    moved: service and session event interfaces

---
+++ Models/Interfaces/ServiceEventInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace HexMakina\koral\Models\Interfaces\Interfaces;
+
+use HexMakina\kadro\Models\Interfaces\EventInterface;
+
+// implementation in Models\Abilities\ServiceEvent
+interface ServiceEventInterface extends EventInterface
+{
+  public function event_service();        // returns the service
+  public function event_service_field();  // return the service field name
+}
+
+?>
---
+++ Models/Interfaces/SessionEventInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace HexMakina\koral\Models\Interfaces\Interfaces;
+
+// implementation in Models\Abilities\SessionEvent
+interface SessionEventInterface extends ServiceEventInterface
+{
+  public function event_session_field();
+  public function event_session();
+}
+
+?>
