1247aa918cb8ef1b389538a82c33609af731ab9f
sam <sammy@hexmakina.be>
2021-09-09 12:27:20 +0200

    type: target_conroller

--- Controllers/Home.php
+++ Controllers/Home.php
@@ -14,26 +14,26 @@ class Home extends \HexMakina\kadro\Controllers\Kadro
 
     public function bootstrap()
     {
-        $target_conroller = $this->get('HexMakina\BlackBox\RouterInterface')->targetController();
-        $target_conroller = $this->get('Controllers\\' . $target_conroller);
+        $target_controller = $this->get('HexMakina\BlackBox\RouterInterface')->targetController();
+        $target_controller = $this->get('Controllers\\' . $target_controller);
 
-        if (!$target_conroller->get('HexMakina\BlackBox\StateAgentInterface')->hasFilter('date_start')) {
-            $target_conroller->get('HexMakina\BlackBox\StateAgentInterface')->filters('date_start', Dato::format($target_conroller->get('settings.app.time_window_start'), Dato::FORMAT));
+        if (!$target_controller->get('HexMakina\BlackBox\StateAgentInterface')->hasFilter('date_start')) {
+            $target_controller->get('HexMakina\BlackBox\StateAgentInterface')->filters('date_start', Dato::format($target_controller->get('settings.app.time_window_start'), Dato::FORMAT));
         }
 
-        if (!$target_conroller->get('HexMakina\BlackBox\StateAgentInterface')->hasFilter('date_stop')) {
-            $target_conroller->get('HexMakina\BlackBox\StateAgentInterface')->filters('date_stop', Dato::format($target_conroller->get('settings.app.time_window_stop'), Dato::FORMAT));
+        if (!$target_controller->get('HexMakina\BlackBox\StateAgentInterface')->hasFilter('date_stop')) {
+            $target_controller->get('HexMakina\BlackBox\StateAgentInterface')->filters('date_stop', Dato::format($target_controller->get('settings.app.time_window_stop'), Dato::FORMAT));
         }
 
-        $this->common_viewport($target_conroller);
-        $target_conroller->execute($this->get('HexMakina\BlackBox\RouterInterface')->targetMethod());
+        $this->common_viewport($target_controller);
+        $target_controller->execute($this->get('HexMakina\BlackBox\RouterInterface')->targetMethod());
     }
 
-    public function common_viewport($target_conroller)
+    public function common_viewport($target_controller)
     {
         $all_operators = Operator::filter();
-        $target_conroller->viewport('all_operators', $all_operators);
-        $target_conroller->viewport('services', $target_conroller->get('Models\Service::class')::filter());
-        $target_conroller->viewport('CurrentOperator', $this->get('HexMakina\BlackBox\Auth\OperatorInterface'));
+        $target_controller->viewport('all_operators', $all_operators);
+        $target_controller->viewport('services', $target_controller->get('Models\Service::class')::filter());
+        $target_controller->viewport('CurrentOperator', $this->get('HexMakina\BlackBox\Auth\OperatorInterface'));
     }
 }
